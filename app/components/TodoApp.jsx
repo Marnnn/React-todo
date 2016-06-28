7c856c5dbe6e867e50b61d90d94cd448cb9e8018
Graham Test Fletcher <GrFletcher@draperanddash.local>
2016-06-28 17:53:03 +0100

    Add Todo component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        }, {
+          id: 2,
+          text: 'Clean the yard'
+        }
+      ]
+    };
+  },
+  render: function () {
+    return (
+      <div>
+        TodoApp.jsx
+      </div>
+    )
+  }
+});
+
+module.exports = TodoApp;
