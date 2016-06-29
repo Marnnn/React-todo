1a0f3e70e1d276eb7e6e3a49dacdd08c2bb8850c
Marnnn <marnburger@gmail.com>
2016-06-29 22:00:48 +0100

    Run webpack on heroku

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -32,7 +32,7 @@ export var startAddTodo = (text) => {
     };
     var todoRef = firebaseRef.child('todos').push(todo);
 
-    todoRef.then(() => {
+    return todoRef.then(() => {
       dispatch(addTodo({
         ...todo,
         id: todoRef.key
@@ -48,9 +48,44 @@ export var addTodos = (todos) => {
   };
 };
 
-export var toggleTodo = (id) => {
+export var startAddTodos = () => {
+  return (dispatch, getState) => {
+    var todosRef = firebaseRef.child('todos');
+    return todosRef.once('value').then((snapshot) => {
+      var todos = snapshot.val() || {};
+      var parsedTodos = [];
+
+      Object.keys(todos).forEach((todoId) => {
+        parsedTodos.push({
+          id: todoId,
+          ...todos[todoId]
+        });
+      });
+
+      dispatch(addTodos(parsedTodos));
+    });
+  };
+};
+
+//Object.keys(todos)
+
+export var updateTodo = (id, updates) => {
   return {
-    type: 'TOGGLE_TODO',
-    id
+    type: 'UPDATE_TODO',
+    id,
+    updates
+  };
+};
+
+export var startToggleTodo = (id, completed) => {
+  return (dispatch, getState) => {
+    var todoRef = firebaseRef.child('todos/' + id);
+    var updates = {
+      completed,
+      completedAt: completed ? moment().unix() : null
+    };
+    return todoRef.update(updates).then(() => {
+      dispatch(updateTodo(id, updates));
+    });
   };
 };
